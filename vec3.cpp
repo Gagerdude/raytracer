dc86b50b12bf809a93b1f966be03858465f6098d
Gage Phillips <gagerdude@gmail.com>
2019-05-29 21:07:04 -0500

    finished constructors

---
+++ vec3.cpp
@@ -0,0 +1,137 @@
+#include "vec3.h"
+
+#include <stdexcept>
+
+vec3::vec3(){
+	this->m_array[0] = 0;
+	this->m_array[1] = 0;
+	this->m_array[2] = 0;
+}
+
+vec3::vec3(float v1, float v2, float v3){
+	this->m_array[0] = v1;
+	this->m_array[1] = v2;
+	this->m_array[2] = v3;
+}
+
+vec3::vec3(const vec3& vec){
+	this->m_array[0] = vec.m_array[0];
+	this->m_array[1] = vec.m_array[1];
+	this->m_array[2] = vec.m_array[2];
+}
+
+vec3& vec3::operator=(const vec3& vec){
+	this->m_array[0] = vec.m_array[0];
+	this->m_array[1] = vec.m_array[1];
+	this->m_array[2] = vec.m_array[2];
+}
+
+vec3 vec3::operator-(){
+	vec3 negatedVector(
+        -1 * this->m_array[0],
+        -1 * this->m_array[1],
+        -1 * this->m_array[2]
+    );
+
+    return negatedVector;
+}
+
+float vec3::operator[](int index) const{
+	if(index < 0 || 3 <= index){
+        throw std::out_of_range("Index");
+    }
+
+    return this->m_array[index];
+}
+
+float& vec3::operator[](int index){
+	if(index < 0 || 3 <= index){
+        throw std::out_of_range("Index");
+    }
+
+    return this->m_array[index];
+}
+
+vec3 vec3::operator+(const vec3& rhs){
+
+}
+
+vec3 vec3::operator-(const vec3& rhs){
+
+}
+
+vec3 vec3::operator*(const vec3& rhs){
+
+}
+
+vec3 vec3::operator/(const vec3& rhs){
+
+}
+
+vec3 vec3::operator*(float rhs){
+
+}
+
+vec3 vec3::operator/(float rhs){
+
+}
+
+vec3& vec3::operator+=(const vec3& rhs){
+	
+}
+
+vec3& vec3::operator-=(const vec3& rhs){
+	
+}
+
+vec3& vec3::operator*=(const vec3& rhs){
+	
+}
+
+vec3& vec3::operator/=(const vec3& rhs){
+	
+}
+
+vec3& vec3::operator*=(float rhs){
+	
+}
+
+vec3& vec3::operator/=(float rhs){
+	
+}
+
+float vec3::x() const{
+	
+}
+
+float vec3::y() const{
+	
+}
+
+float vec3::z() const{
+	
+}
+
+float vec3::r() const{
+	
+}
+
+float vec3::g() const{
+	
+}
+
+float vec3::b() const{
+	
+}
+
+float vec3::length() const{
+	
+}
+
+float vec3::length_squared() const{
+	
+}
+
+vec3& vec3::normalize(){
+	
+}
