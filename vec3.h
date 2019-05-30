5fdf170eaaf7302c474115482d7c536480b1ddba
Gage Phillips <gagerdude@gmail.com>
2019-05-29 21:04:57 -0500

    added +-*/ operators

--- vec3.h
+++ vec3.h
@@ -17,6 +17,14 @@ class vec3{
         float operator[](int index) const;
         float& operator[](int index);
 
+        vec3 operator+(const vec3& rhs);
+        vec3 operator-(const vec3& rhs);
+        vec3 operator*(const vec3& rhs);
+        vec3 operator/(const vec3& rhs);
+        
+        vec3 operator*(float rhs);
+        vec3 operator/(float rhs);
+
         vec3& operator+=(const vec3& rhs);
         vec3& operator-=(const vec3& rhs);
         vec3& operator*=(const vec3& rhs);
