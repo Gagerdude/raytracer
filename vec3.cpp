8f6b3b25470c5008bc9351f0217a8699ee90866f
Gage Phillips <gagerdude@gmail.com>
2019-05-29 21:37:14 -0500

    Added dot and cross functions

--- vec3.cpp
+++ vec3.cpp
@@ -212,3 +212,19 @@ vec3& vec3::normalize(){
 
     return *this;
 }
+
+float dot(const vec3& lhs, const vec3& rhs){
+    return (
+        lhs.x() * rhs.x() +
+        lhs.y() * rhs.y() +
+        lhs.z() * rhs.z()
+    );
+}
+
+vec3 cross(const vec3& lhs, const vec3& rhs){
+    return vec3(
+        lhs.y() * rhs.z() - lhs.z() * rhs.y(),
+        -1 * (lhs.x() * rhs.z() - lhs.z() * rhs.x()),
+        lhs.x() * rhs.y() - lhs.y() * rhs.x() 
+    );
+}
