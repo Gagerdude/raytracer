9b11421991c2da7490caa193073dcec47b60fd56
Gage Phillips <gagerdude@gmail.com>
2019-06-22 20:58:11 -0500

    Added trilinear interpolation to the noise

--- textures/Perlin.cpp
+++ textures/Perlin.cpp
@@ -4,12 +4,43 @@
 
 #include <random>
 
-double Perlin::noise(const vec3& p) const{
-    int i = int(4 * p.x()) & 255;
-    int j = int(4 * p.y()) & 255;
-    int k = int(4 * p.z()) & 255;
+double triliniear_interpolate(double c[2][2][2], double u, double v, double w){
+    double sum = 0;
 
-    return randouble[perm_x[i] ^ perm_y[j] ^ perm_z[k]];
+    for(int i = 0; i < 2; i++){
+        for(int j = 0; j < 2; j++){
+            for(int k = 0; k < 2; k++){
+                sum += (i*u + (1 - i) * (1 - u)) *
+                       (j*v + (1 - j) * (1 - v)) *
+                       (k*w + (1 - k) * (1 - w)) *
+                       c[i][j][k];
+            }
+        }
+    }
+
+    return sum;
+}
+
+double Perlin::noise(const vec3& p) const{
+    int i = std::floor(p.x());
+    int j = std::floor(p.y());
+    int k = std::floor(p.z());
+
+    double u = p.x() - std::floor(p.x());
+    double v = p.y() - std::floor(p.y());
+    double w = p.z() - std::floor(p.z());
+
+    double c[2][2][2];
+
+    for(int di = 0; di < 2; di++){
+        for(int dj = 0; dj < 2; dj++){
+            for(int dk = 0; dk < 2; dk++){
+                c[di][dj][dk] = randouble[perm_x[(i+di) & 255] ^ perm_y[(j+dj) & 255] ^ perm_z[(k+dk) & 255]];
+            }
+        }
+    }
+
+    return triliniear_interpolate(c, u, v, w);
 }
 
 static double* perlin_generate(){
