f2042ec01c674c9915b4d33cc8055782a408e454
Gage Phillips <gagerdude@gmail.com>
2019-06-23 01:27:10 -0500

    Added turbulence calcs

--- textures/Perlin.cpp
+++ textures/Perlin.cpp
@@ -4,16 +4,21 @@
 
 #include <random>
 
-double triliniear_interpolate(double c[2][2][2], double u, double v, double w){
+double perlin_interpolate(vec3 c[2][2][2], double u, double v, double w){
     double sum = 0;
+    
+    double uu = u * u * (3 - (2 * u));
+    double vv = v * v * (3 - (2 * v));
+    double ww = w * w * (3 - (2 * w));
 
     for(int i = 0; i < 2; i++){
         for(int j = 0; j < 2; j++){
             for(int k = 0; k < 2; k++){
-                sum += (i*u + (1 - i) * (1 - u)) *
-                       (j*v + (1 - j) * (1 - v)) *
-                       (k*w + (1 - k) * (1 - w)) *
-                       c[i][j][k];
+                vec3 weight_vec(u-i, v-j, w-k);
+                sum += (i*uu + (1 - i) * (1 - uu)) *
+                       (j*vv + (1 - j) * (1 - vv)) *
+                       (k*ww + (1 - k) * (1 - ww)) *
+                       dot(c[i][j][k], weight_vec);
             }
         }
     }
@@ -22,33 +27,46 @@ double triliniear_interpolate(double c[2][2][2], double u, double v, double w){
 }
 
 double Perlin::noise(const vec3& p) const{
-    int i = std::floor(p.x());
-    int j = std::floor(p.y());
-    int k = std::floor(p.z());
-
     double u = p.x() - std::floor(p.x());
     double v = p.y() - std::floor(p.y());
     double w = p.z() - std::floor(p.z());
 
-    double c[2][2][2];
+    int i = std::floor(p.x());
+    int j = std::floor(p.y());
+    int k = std::floor(p.z());
+
+    vec3 c[2][2][2];
 
     for(int di = 0; di < 2; di++){
         for(int dj = 0; dj < 2; dj++){
             for(int dk = 0; dk < 2; dk++){
-                c[di][dj][dk] = randouble[perm_x[(i+di) & 255] ^ perm_y[(j+dj) & 255] ^ perm_z[(k+dk) & 255]];
+                c[di][dj][dk] = ranvec[perm_x[(i+di) & 255] ^ perm_y[(j+dj) & 255] ^ perm_z[(k+dk) & 255]];
             }
         }
     }
 
-    return triliniear_interpolate(c, u, v, w);
+    return perlin_interpolate(c, u, v, w);
 }
 
-static double* perlin_generate(){
-    double* rand_array = new double[256];
-    std::uniform_real_distribution<double> dist(0, 1);
+double Perlin::turbulence(vec3 p, int depth) const{
+    double sum = 0;
+    double weight = 1;
+
+    for(int i = 0; i < depth; i++){
+        sum += weight * noise(p);
+        weight *= .5;
+        p *= 2;
+    }
+
+    return std::abs(sum);
+}
+
+static vec3* perlin_generate(){
+    vec3* rand_array = new vec3[256];
 
     for(int i = 0; i < 256; i++){
-        rand_array[i] = dist(Raytracer::rng);
+        rand_array[i] = vec3::random();
+        std::cout << rand_array[i] << std::endl;
     }
 
     return rand_array;
@@ -75,7 +93,7 @@ static int* perlin_generate_permutation(){
     return array;
 }
 
-double* Perlin::randouble = perlin_generate();
+vec3* Perlin::ranvec = perlin_generate();
 int* Perlin::perm_x = perlin_generate_permutation();
 int* Perlin::perm_y = perlin_generate_permutation();
 int* Perlin::perm_z = perlin_generate_permutation();
--- textures/Perlin.h
+++ textures/Perlin.h
@@ -6,8 +6,9 @@
 class Perlin{
     public:
         double noise(const vec3& p) const;
+        double turbulence(vec3 p, int depth = 7) const;
 
-        static double* randouble;
+        static vec3* ranvec;
         static int* perm_x;
         static int* perm_y;
         static int* perm_z;
