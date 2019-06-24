03897c62c4f72226799e8980ed4a4a9e392a2e57
Gage Phillips <gagerdude@gmail.com>
2019-06-23 22:47:43 -0500

    Now allows materials to emit their own light

--- materials/Material.h
+++ materials/Material.h
@@ -8,6 +8,9 @@ class Material {
     public:
         virtual ~Material(){}
         virtual bool scatter(const Ray& ray_in, const hit_record& rec, vec3& attenuation, Ray& ray_scattered) const = 0;
+        virtual vec3 emitted(float u, float v, const vec3& p) const{
+            return vec3(0);
+        }
 };
 
 #endif
